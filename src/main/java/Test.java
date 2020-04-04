c242e409642ee071ceafd433d6fe36c779c431f6
Rainard <rainard.human@gmail.com>
2020-04-04 18:25:47 +0200

    Dislpay Screen & Block !!

---
+++ src/main/java/Test.java
@@ -0,0 +1,47 @@
+public class Test {
+    static Block board[];
+    public static void main(String[] args) {
+        int[][] layout = {
+                {0, 4, 0, 0, 0, 0, 1, 7, 9},
+                {0, 0, 2, 0, 0, 8, 0, 5, 4},
+                {0, 0, 2, 0, 0, 8, 0, 5, 4},
+                {0, 0, 6, 0, 0, 5, 0, 0, 8},
+                {0, 8, 0, 0, 7, 0, 9, 1, 0},
+                {0, 5, 0, 0, 9, 0, 0, 3, 0},
+                {0, 1, 9, 0, 6, 0, 0, 4, 0},
+                {3, 0, 0, 4, 0, 0, 7, 0, 0},
+                {5, 7, 0, 1, 0, 0, 2, 0, 0},
+                {9, 2, 8, 0, 0, 0, 0, 6, 0}};
+
+        board = new Block[81];
+        DisplayScreen screen = new DisplayScreen(9,9,'6');
+        int x = 0;
+
+        for (int i = 0; i < 9 ; i++) {
+            for (int j = 0; j < 9; j++) {
+                board[x] = new Block(j,i,layout[i][j]);
+                //screen.setPixel_value(i,j,Integer.toString(layout[i][j]).charAt(0));
+                x++;
+            }
+
+        }
+
+
+
+
+
+
+        System.out.println(screen);
+
+
+
+    }
+
+    public static void  displayBoard(){
+
+        System.out.println("Check: " + board[7].block);
+        System.out.println("Check: " + board[7].x);
+        System.out.println("Check: " + board[7].y);
+        System.out.println("Check: " + board[7].value);
+    }
+}
